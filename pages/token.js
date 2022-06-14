5b3089d8223f65bd4a20cba408ec8cd073d780f0
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2022-06-14 17:06:01 -0400

    Update token.js

--- pages/token.js
+++ pages/token.js
@@ -18,11 +18,11 @@ const Token = () => (
         <Section>
 
             <Box align="center" my={4}>
-            <BioToken>            <iframe
-            src="https://gateway.ipfscdn.io/ipfs/QmfJu3spsSJot6givCK2VjwEgVHymc5RCXHqfG1W5WZyFX/token-drop.html?contract=0x4d9EdfCBBECD8664174e0C684B7eb180769cCa06&chainId=4"
-            width="475px"
-            height="470px">
-            </iframe>
+            <BioToken>
+              <iframe
+                src="https://gateway.ipfscdn.io/ipfs/QmfJu3spsSJot6givCK2VjwEgVHymc5RCXHqfG1W5WZyFX/token-drop.html?contract=0x30B7Fb8EeC511e7EA763A6eD3c6eD58795cB05E2&chainId=137"
+                width="475"
+                height="470"></iframe>
             </BioToken>
         </Box>
 
