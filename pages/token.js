96058279006b164113df6816bc41b1379302acc4
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2022-06-14 15:55:24 -0400

    Update token.js

--- pages/token.js
+++ pages/token.js
@@ -1,26 +1,31 @@
 import { Container, Heading, SimpleGrid, Divider, Box } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
+import {  BioToken } from '../components/bio'
 
 
 const Token = () => (
   <Layout title="ERM's Token">
     <Container>
-      <Heading as="h3" fontSize={20} mb={4} textAlign="center">
-        ERM's Tokens
-      </Heading>
+    <Box flexGrow={4}>
+                <Heading as="h2" variant='page-title'>
+                                ERM's Token
+                </Heading>
+            <p>My own Crypto token. It will be used wisely for my future projects related to Web3...</p>
+            </Box>
 
       <SimpleGrid >
         <Section>
-        <div>
-        <iframe
-            src="https://gateway.ipfscdn.io/ipfs/QmfJu3spsSJot6givCK2VjwEgVHymc5RCXHqfG1W5WZyFX/token-drop.html?contract=0x4d9EdfCBBECD8664174e0C684B7eb180769cCa06&chainId=4"
-            width="510px"
-            height="420px"
-            float="left">
 
+            <Box align="center" my={4}>
+            <BioToken>            <iframe
+            src="https://gateway.ipfscdn.io/ipfs/QmfJu3spsSJot6givCK2VjwEgVHymc5RCXHqfG1W5WZyFX/token-drop.html?contract=0x4d9EdfCBBECD8664174e0C684B7eb180769cCa06&chainId=4"
+            width="475px"
+            height="470px">
             </iframe>
-        </div>
+            </BioToken>
+        </Box>
+
         </Section>
 
 
