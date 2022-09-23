2d1c7597e52d9d7d779af575e25281a16f787bbb
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2022-09-22 20:11:50 -0400

    Update the capitals word of it into IT

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const Page = () => {
                 <Heading as="h2" variant='page-title'>
                     Melbouci E. Rayane
                 </Heading>
-            <p>It Support(It / Developper / Designer)</p>
+            <p>IT Support(IT / Developper / Designer)</p>
             </Box>
            <Box flexShrink={0} mt={{base: 4, md:0}} ml={{md: 6}} align="center">
                <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/images/rayane.jpg" alt="Profile Image"/>
