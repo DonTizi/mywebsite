bbe322f9535c1051d30a1e8c20c58f780761c338
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2023-07-23 00:58:21 -0400

    Update EvaAI.js

--- pages/works/EvaAI.js
+++ pages/works/EvaAI.js
@@ -16,7 +16,7 @@ const Work = () => (
       <List ml={4} my={4}>
         <ListItem>
           <Meta>In production.</Meta>
-          <Link>
+          <Link href="https://www.evahyp.com/">>
           Version 1.0.0 <ExternalLinkIcon mx="2px" />
           </Link>
         </ListItem>
